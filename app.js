4c1859d9532921320808765b2e9d006f5f24f197
Kalpeshhp <kalpeshpatilh2001@gmail.com>
2025-08-06 22:41:47 +0530

    minor changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongo.net/superData', {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
